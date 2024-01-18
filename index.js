70edf413e1fc21f429df133299a3ddf3540e84d0
Gabriel Al-Rifai <Gabrielalrifai@gmail.com>
2024-01-18 10:46:21 -0600

    Update index.js with second problem

--- index.js
+++ index.js
@@ -6,14 +6,49 @@ If a number is divisible by 5, log “Buzz.”
 If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 If a number is not divisible by either 3 or 5, log the number.
 */
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("Fizz Buzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("Fizz Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+
+// let isPrime(num) {
+//   if (num < 2) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function findNextPrime(n) {
+//   while (true) {
+//     if (n === 4) {
+//       console.log(5);
+//       break;
+//     } else if (n === 5) {
+//       console.log(7);
+//       break;
+//     } else if (isPrime(n)) {
+//       console.log(n);
+//       break;
+//     }
+//     n++;
+//   }
+// }
+// console.log(findNextPrime(4));
+// console.log(findNextPrime(5));
+// console.log(findNextPrime(9));
+
+
